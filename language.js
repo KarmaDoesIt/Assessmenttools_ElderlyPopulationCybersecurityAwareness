fb5772f2abad4841c13628c379982c0320d1436e
Tania Quinones <149525475+KarmaDoesIt@users.noreply.github.com>
2025-04-15 14:33:54 -0400

    Create language.js

---
+++ language.js
@@ -0,0 +1,12 @@
+<script>
+    function changeLanguage(lang) {
+        // Assuming you have index_en.html, index_es.html, etc.
+        if (lang === 'en') {
+            window.location.href = 'index_en.html';
+        } else if (lang === 'es') {
+            window.location.href = 'index_es.html';
+        } else if (lang === 'fr') {
+            window.location.href = 'index_fr.html';
+        }
+    }
+</script>
