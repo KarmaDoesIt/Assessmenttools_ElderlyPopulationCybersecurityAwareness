3fc99322d6e5bb3b918431efb4009a3962916fcc
Yanisley Mendiola <117114439+ymendiola17@users.noreply.github.com>
2025-04-24 01:04:23 -0400

    Create langSwitch.js

---
+++ langSwitch.js
@@ -0,0 +1,13 @@
+  <script>
+    document.getElementById("languageSwitcher").addEventListener("change", function () {
+      window.location.href = this.value;
+    });
+
+    const currentPage = location.pathname.split("/").pop();
+    const options = document.querySelectorAll("#languageSwitcher option");
+    options.forEach(option => {
+      if (option.value === currentPage) {
+        option.selected = true;
+      }
+    });
+  </script>
