0e2a9905971ce75af2a59da7ec0583dbb2ea354d
Yanisley Mendiola <117114439+ymendiola17@users.noreply.github.com>
2025-04-24 01:11:31 -0400

    Update langSwitch.js

--- langSwitch.js
+++ langSwitch.js
@@ -1,13 +1,11 @@
-  <script>
-    document.getElementById("languageSwitcher").addEventListener("change", function () {
-      window.location.href = this.value;
-    });
+document.getElementById("languageSwitcher").addEventListener("change", function () {
+  window.location.href = this.value;
+});
 
-    const currentPage = location.pathname.split("/").pop();
-    const options = document.querySelectorAll("#languageSwitcher option");
-    options.forEach(option => {
-      if (option.value === currentPage) {
-        option.selected = true;
-      }
-    });
-  </script>
+const currentPage = location.pathname.split("/").pop();
+const options = document.querySelectorAll("#languageSwitcher option");
+options.forEach(option => {
+  if (option.value === currentPage) {
+    option.selected = true;
+  }
+});
